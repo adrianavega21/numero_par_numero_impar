d64b59a7cd57ed25e9772a202c54bfb3bc719b3c
adrianavega21 <85266547+adrianavega21@users.noreply.github.com>
2021-06-07 21:22:53 -0500

    Ejercicio numero par y numero impar
    Ejercicio para saber si un numero es par o impar.

---
+++ ejercicio006.py
@@ -0,0 +1,7 @@
+#Ingrese un numero y diga si el numero es par o impar 
+num_ing=int(input('Hola!! Ingrese un numero para saber si es par o impar: \t'))
+if num_ing % 2 == 0:
+    print('es par')
+else:
+    print('es impar')
+
